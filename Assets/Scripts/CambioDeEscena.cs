8ddded3c7cc08c80a64e4b90a232598b65eb3aa3
zyzy-A01424047 <84796996+zyzy-A01424047@users.noreply.github.com>
2021-06-10 15:23:49 -0500

    Videos listos

--- Assets/Scripts/CambioDeEscena.cs
+++ Assets/Scripts/CambioDeEscena.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,3 +35,41 @@ public class CambioDeEscena : MonoBehaviour
         }
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeEscena : MonoBehaviour
+{
+    private int score;
+
+    public float timeLeft;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        timeLeft = 10f; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            score++; 
+        }
+
+        timeLeft = timeLeft - Time.deltaTime;
+        
+        if(timeLeft< 0)
+        {
+            SceneManager.LoadScene("Sala Final");
+        }
+    }
+}
+>>>>>>> Stashed changes
