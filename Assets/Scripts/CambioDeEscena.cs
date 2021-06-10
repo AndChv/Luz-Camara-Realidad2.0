279a0c5b7d02da2d33a49c56606372b62a0942fa
Ghostman9515 <A01706603@itesm.mx>
2021-06-10 11:07:07 -0500

    cambio de escena

---
+++ Assets/Scripts/CambioDeEscena.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeEscena : MonoBehaviour
+{
+    private int score;
+
+    public float timeLeft;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        timeLeft = 180f; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            score++; 
+        }
+
+        timeLeft = timeLeft - Time.deltaTime;
+        
+        if(timeLeft< 0)
+        {
+            SceneManager.LoadScene("Sala Final");
+        }
+    }
+}
